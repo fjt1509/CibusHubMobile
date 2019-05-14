386afe3038c18ac023df66d41443f8c85a2459c1
KentJP <32635044+KentJP@users.noreply.github.com>
2019-05-14 15:51:00 +0200

    Addactivity, camera

--- app/src/main/java/com/example/cibushub/AddPostActivity.java
+++ app/src/main/java/com/example/cibushub/AddPostActivity.java
@@ -1,13 +1,147 @@
 package com.example.cibushub;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.example.cibushub.BE.Post;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
-public class AddPostActivity extends AppCompatActivity {
+public class AddPostActivity extends AppCompatActivity  {
+    private final static String LOGTAG = "Camtag";
+    private final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    File mFile;
+    Post p;
+    ImageView imageTaken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkPermissions();
         setContentView(R.layout.activity_add_post);
+        imageTaken = findViewById(R.id.imageTaken);
+        p = (Post) getIntent().getSerializableExtra("post");
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+        findViewById(R.id.btnTakePhoto).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TakePhoto();
+            }
+        });
     }
+
+
+    private void TakePhoto()
+    {
+
+        mFile = getOutputMediaFile(); // create a file to save the image
+        if (mFile == null)
+        {
+            Toast.makeText(this, "Could not create file...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        // create Intent to take a picture
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mFile));
+
+        Log.d(LOGTAG, "file uri = " + Uri.fromFile(mFile).toString());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            Log.d(LOGTAG, "camera app will be started");
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+        else
+            Log.d(LOGTAG, "camera app could NOT be started");
+
+    }
+
+
+
+
+    void log(String s) {
+        Log.d(LOGTAG, s);
+    }
+
+    private File getOutputMediaFile(){
+
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "Camera01");
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                log("failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String postfix = "jpg";
+        String prefix = "IMG";
+
+        File mediaFile = new File(mediaStorageDir.getPath() +
+                File.separator + prefix +
+                "_"+ timeStamp + "." + postfix);
+
+        return mediaFile;
+    }
+
+    private void showPictureTaken(File f) {
+
+        imageTaken.setImageURI(Uri.fromFile(f));
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                showPictureTaken(mFile);
+
+            } else
+            if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Canceled...", Toast.LENGTH_LONG).show();
+                return;
+
+            } else
+                Toast.makeText(this, "Picture NOT taken - unknown error...", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void checkPermissions() {
+        ArrayList<String> permissions = new ArrayList<String>();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+            permissions.add(Manifest.permission.CAMERA);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+            permissions.add(Manifest.permission.CALL_PHONE);
+
+
+        if (permissions.size() > 0)
+            ActivityCompat.requestPermissions(this, permissions.toArray(new String[permissions.size()]), 1);
+    }
+
+
 }
